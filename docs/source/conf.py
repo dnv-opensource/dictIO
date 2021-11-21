da525dbf50903b5c4108a6a8599f177075fc6408
Claas <claas.rostock@dnvgl.com>
2021-11-21 17:54:26 +0100

    bumped version to 0.0.9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, DNV. Frank Lumpitzsch, Claas Rostock, Seung Hyeon Yoo'
 author = 'Frank Lumpitzsch, Claas Rostock, Seung Hyeon Yoo'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.8'
+release = '0.0.9'
 
 # -- General configuration ---------------------------------------------------
 
