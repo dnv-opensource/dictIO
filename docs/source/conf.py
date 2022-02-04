8006af7e1170783835d19a02a1188bb0da2bfc63
Claas <claas.rostock@dnvgl.com>
2022-02-04 17:49:49 +0100

    bumped version number to 0.0.15

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, DNV. Frank Lumpitzsch, Claas Rostock, Seung Hyeon Yoo'
 author = 'Frank Lumpitzsch, Claas Rostock, Seung Hyeon Yoo'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.14'
+release = '0.0.15'
 
 # -- General configuration ---------------------------------------------------
 
