a060d001583272d7c498394224bbf13161d03209
Claas <claas.rostock@dnvgl.com>
2022-05-13 08:46:28 +0200

    tests/conftest.py: minor name changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,18 +10,18 @@ def chdir():
     os.chdir(Path(__file__).parent.absolute() / 'test_dicts')
 
 
-parsed_files = ['parsed.*']
+dictIO_files = ['parsed.*']     # noqa
 
 
 @pytest.fixture(autouse=True)
 def default_setup_and_teardown():
-    _remove_parsed_files()
+    _remove_dictIO_files()
     yield
-    _remove_parsed_files()
+    _remove_dictIO_files()
 
 
-def _remove_parsed_files():
-    for pattern in parsed_files:
+def _remove_dictIO_files():     # noqa
+    for pattern in dictIO_files:
         for file in glob(pattern):
             file = Path(file)
             file.unlink(missing_ok=True)
