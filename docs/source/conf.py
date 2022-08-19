4c6b8c173efc6e10123c782d705e40b398dbc40a
Claas <claas.rostock@dnvgl.com>
2022-08-19 12:43:39 +0200

    bumped version number to 0.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, DNV. Frank Lumpitzsch, Claas Rostock, Seung Hyeon Yoo'
 author = 'Frank Lumpitzsch, Claas Rostock, Seung Hyeon Yoo'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 # -- General configuration ---------------------------------------------------
 
