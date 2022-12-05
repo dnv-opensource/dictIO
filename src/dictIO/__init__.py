f014ae352df2d1f56230b59b92512b7497348fea
Claas <claas.rostock@dnvgl.com>
2022-12-05 22:40:54 +0100

    isort: configured to skip __init__.py files

--- src/dictIO/__init__.py
+++ src/dictIO/__init__.py
@@ -5,9 +5,6 @@ from .cppDict import (  # noqa: F401
     order_keys,
     set_global_key,
 )
-from .dictParser import DictParser  # noqa: F401
-from .dictReader import DictReader  # noqa: F401
-from .dictWriter import DictWriter, create_target_file_name  # noqa: F401
 from .formatter import (  # noqa: F401
     CppFormatter,
     FoamFormatter,
@@ -16,3 +13,6 @@ from .formatter import (  # noqa: F401
     XmlFormatter,
 )
 from .parser import CppParser, FoamParser, JsonParser, Parser, XmlParser  # noqa: F401
+from .dictReader import DictReader  # noqa: F401
+from .dictWriter import DictWriter, create_target_file_name  # noqa: F401
+from .dictParser import DictParser  # noqa: F401
