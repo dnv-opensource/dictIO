567b33f5d1df31ddcef66ce364d5d7894646f818
Claas <claas.rostock@dnvgl.com>
2022-12-08 20:13:46 +0100

    dictIO/__init__.py : ensured that imported symbols get also exported

--- src/dictIO/__init__.py
+++ src/dictIO/__init__.py
@@ -1,25 +1,32 @@
 from dictIO.cppDict import (  # noqa: F401
-    CppDict,
-    find_global_key,
-    global_key_exists,
-    order_keys,
-    set_global_key,
+    CppDict as CppDict,
+    find_global_key as find_global_key,
+    global_key_exists as global_key_exists,
+    order_keys as order_keys,
+    set_global_key as set_global_key,
 )
 from dictIO.formatter import (  # noqa: F401
-    CppFormatter,
-    FoamFormatter,
-    Formatter,
-    JsonFormatter,
-    XmlFormatter,
+    Formatter as Formatter,
+    CppFormatter as CppFormatter,
+    FoamFormatter as FoamFormatter,
+    JsonFormatter as JsonFormatter,
+    XmlFormatter as XmlFormatter,
 )
 from dictIO.parser import (  # noqa: F401
-    CppParser,
-    FoamParser,
-    JsonParser,
-    Parser,
-    XmlParser,
+    Parser as Parser,
+    CppParser as CppParser,
+    FoamParser as FoamParser,
+    JsonParser as JsonParser,
+    XmlParser as XmlParser,
 )
 
-from dictIO.dictReader import DictReader  # noqa: F401
-from dictIO.dictWriter import DictWriter, create_target_file_name  # noqa: F401
-from dictIO.dictParser import DictParser  # noqa: F401
+from dictIO.dictReader import (  # noqa: F401
+    DictReader as DictReader,
+)
+from dictIO.dictWriter import (  # noqa: F401
+    DictWriter as DictWriter,
+    create_target_file_name as create_target_file_name,
+)
+from dictIO.dictParser import (  # noqa: F401
+    DictParser as DictParser,
+)
