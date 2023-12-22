beaad103d7c7b09815cc3fdf4cd98a389e2fea25
Claas <claas.rostock@dnv.com>
2023-12-22 09:58:09 +0100

    bumped version number to 0.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, DNV. Frank Lumpitzsch, Claas Rostock, Seung Hyeon Yoo"
 author = "Frank Lumpitzsch, Claas Rostock, Seung Hyeon Yoo"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.9"
+release = "0.3.0"
 
 # -- General configuration ---------------------------------------------------
 
