ada1d4b79c3f7036fe2d21975ae78aa7d4d08e04
Claas <ClaasRostock@users.noreply.github.com>
2024-01-09 13:22:14 +0100

    README and LICENSE: updated '2023' to '2024'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath("../../src"))
 # -- Project information -----------------------------------------------------
 
 project = "dictIO"
-copyright = "2023, DNV. Frank Lumpitzsch, Claas Rostock, Seung Hyeon Yoo"
+copyright = "2024, DNV. Frank Lumpitzsch, Claas Rostock, Seung Hyeon Yoo"
 author = "Frank Lumpitzsch, Claas Rostock, Seung Hyeon Yoo"
 
 # The full version, including alpha/beta/rc tags
