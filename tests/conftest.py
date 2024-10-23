850bc01a839343ee710a03bf4f4767b174dd9995
Claas <claas.rostock@dnv.com>
2024-10-23 19:36:35 +0200

    tests/conftest.py : added docstrings

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,10 @@ def chdir() -> None:
 
 @pytest.fixture(scope="package", autouse=True)
 def test_dir() -> Path:
+    """
+    Fixture that returns the absolute path of the directory containing the current file.
+    This fixture is automatically used for the entire package.
+    """
     return Path(__file__).parent.absolute()
 
 
@@ -24,12 +28,19 @@ output_files: list[str] = [
 
 @pytest.fixture(autouse=True)
 def default_setup_and_teardown():
+    """
+    Fixture that performs setup and teardown actions before and after each test function.
+    It removes the output directories and files specified in 'output_dirs' and 'output_files' lists.
+    """
     _remove_output_dirs_and_files()
     yield
     _remove_output_dirs_and_files()
 
 
 def _remove_output_dirs_and_files() -> None:
+    """
+    Helper function that removes the output directories and files specified in 'output_dirs' and 'output_files' lists.
+    """
     for folder in output_dirs:
         rmtree(folder, ignore_errors=True)
     for pattern in output_files:
@@ -40,10 +51,15 @@ def _remove_output_dirs_and_files() -> None:
 
 @pytest.fixture(autouse=True)
 def setup_logging(caplog: pytest.LogCaptureFixture) -> None:
+    """
+    Fixture that sets up logging for each test function.
+    It sets the log level to 'INFO' and clears the log capture.
+    """
     caplog.set_level("INFO")
     caplog.clear()
 
 
 @pytest.fixture(autouse=True)
 def logger() -> logging.Logger:
+    """Fixture that returns the logger object."""
     return logging.getLogger()
